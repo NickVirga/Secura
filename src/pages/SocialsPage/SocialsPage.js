13c85f0ebeebaf3d9595b65b209f013da6e0b0e5
Nick Virga <nsvirga@gmail.com>
2023-08-25 11:07:25 -0400

    add axios calls to socials page

--- src/pages/SocialsPage/SocialsPage.js
+++ src/pages/SocialsPage/SocialsPage.js
@@ -1,8 +1,67 @@
 import "./SocialsPage.scss";
 import facebookLogo from "../../assets/images/devicon_facebook.svg";
 import alertsIcon from "../../assets/images/ion_notifications-outline.svg";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 
 function SocialsPage() {
+  const [privacyPolicy, setPrivacyPolicy] = useState("")
+  const [dataCollection, setDataCollection] = useState("")
+  const [deletionSteps, setDeletionSteps] = useState("")
+
+  const reqBodyPrompts = {
+    privacyPrompt: "Explain Facebook's data privacy policy to me in 100 words or less?",
+    dataCollection: "Explain what data Facebook collects from me in 100 words or less?",
+    deletionSteps: "Explain what steps I need to take to delete my data from Facebook in 100 words or less?"
+  }
+
+const privacyClickHandler = () => {
+    const privacyReqBody = {
+      prompt: reqBodyPrompts.privacyPrompt
+    }
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/ask`, privacyReqBody)
+      .then((response) => {
+        console.log(response.data);
+        setPrivacyPolicy(response.data.message)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+}
+
+  
+  // useEffect(() => {
+  //   const collectionReqBody = {
+  //     prompt: reqBodyPrompts.dataCollection
+  //   }
+  //   axios
+  //     .post(`${process.env.REACT_APP_BASE_URL}/ask`, collectionReqBody)
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setDataCollection(response.data.message)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  const stepsClickHandler = () => {
+    const deletionReqBody = {
+      prompt: reqBodyPrompts.deletionSteps
+    }
+    axios
+      .post(`${process.env.REACT_APP_BASE_URL}/ask`, deletionReqBody)
+      .then((response) => {
+        console.log(response.data);
+        setDeletionSteps(response.data.message)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+
+  
   return (
     <>
       <div className="socials__container">
@@ -19,7 +78,7 @@ function SocialsPage() {
           </div>
         </header>
 
-        <h2 className="socials__privacy-title">Facebook's Privacy Policy</h2>
+        <h2 className="socials__privacy-title" onClick={privacyClickHandler}>Facebook's Privacy Policy</h2>
         <div className="socials__alerts-container">
           <h2 className="socials__alerts-header">
             Get alerts on policy changes
@@ -28,7 +87,7 @@ function SocialsPage() {
         </div>
         <h3 className="socials__collection-header">What data is collected?</h3>
         <div className="socials__collection-container">
-          Facebook collects information on user activities, preferences,
+          {/* Facebook collects information on user activities, preferences,
           interactions, connections, and communications. This includes browsing
           and search history, IP address, device and connection type, location,
           content preferences, events attending, parking history, and payment
@@ -36,12 +95,13 @@ function SocialsPage() {
           contacts’ activities, and preferences, including educational history,
           employment history, and interests. Facebook can also track other
           users’ activities in relation to you, such as those who have liked,
-          commented, or interacted with content you have posted.
+          commented, or interacted with content you have posted. */}
+          {dataCollection}
         </div>
         <h3 className="socials__deletion-header">Can this data be deleted?</h3>
         <div className="socials__deletion-explanation">
           Yes! you can delete the data that has been collected by Facebook by
-          following these <span className="socials__deletion-steps">quick and easy steps!</span>
+          following these <span className="socials__deletion-steps" onClick={stepsClickHandler}>quick and easy steps!</span>
         </div>
       </div>
     </>
